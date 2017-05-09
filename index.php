60c8b05ebb47de87d359dae09e0ff53fd008c359
cetecom <cetecom@LC08L5711>
2017-05-09 10:54:56 -0300

    Clase Producto

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+include('lib/Producto.php');
+
+$oProducto1=new Producto();
+$oProducto1->nombre="Nuevo Producto";
+$oProducto1->codigo="001";
+$oProducto2=new Producto();
+$oProducto2->nombre="Nuevo Producto 002";
+$oProducto2->codigo="002";
+$oProducto3=new Producto("Nuevo Producto 003",0,"003");
+
+
+
+
+
+$aProductos[0]=$oProducto1;
+$aProductos[1]=$oProducto2;
+$aProductos[2]=$oProducto3;
+
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -11,7 +32,10 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        foreach ($aProductos as $oPro){
+            echo $oPro->codigo." ".$oPro->nombre;
+            echo "<br>";
+        }
         ?>
     </body>
 </html>
